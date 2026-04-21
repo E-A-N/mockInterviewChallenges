87636922e054053885638d084b46edc53979469a
Cyborg_Ean <ean.director@gmail.com>
2026-04-21 10:47:37 -0700

    Update rust.rs

--- pokemonEvolution/rust.rs
+++ pokemonEvolution/rust.rs
@@ -40,11 +40,11 @@ fn main() {
     };
     poke1.level = 1;
     
-    let poke2 = Pokemon {
+    let mut poke2 = Pokemon {
         name: String::from("Bulbasaur"),
         species_index: 0,
         element_type: ElementTypes::Lightning,
-        level: 15,
+        level: 0,
         stats: Stats {
             attack: 1,
             defense: 1,
@@ -56,4 +56,23 @@ fn main() {
             (32, String::from("Venusaur")),
         ]
     };
+    poke2.level = 2;
+    
+    level_up(&mut poke1);
 }
+
+fn level_up (subject_pokemon: &mut Pokemon) {
+    if subject_pokemon.level >= 99 {
+        return;
+    }
+    
+    subject_pokemon.level += 1;
+    subject_pokemon.stats.attack += 1;
+    subject_pokemon.stats.defense += 1;
+    subject_pokemon.stats.speed += 1;
+    
+    let species_index: &i32 = &subject_pokemon.species_index;
+    let evolution_line: &[(i32, String); 3] = &subject_pokemon.evolution_line;
+    let can_evolve: bool = (*species_index as usize) + 1 < evolution_line.len();
+}
+
