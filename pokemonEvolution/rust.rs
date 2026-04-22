d8e0ea92f05c75a3b5671c8b895bf51d528aed3f
Cyborg_Ean <ean.director@gmail.com>
2026-04-21 20:19:45 -0700

    Update rust.rs

--- pokemonEvolution/rust.rs
+++ pokemonEvolution/rust.rs
@@ -71,8 +71,13 @@ fn level_up (subject_pokemon: &mut Pokemon) {
     subject_pokemon.stats.defense += 1;
     subject_pokemon.stats.speed += 1;
     
-    let species_index: &i32 = &subject_pokemon.species_index;
+    let species_index: usize = subject_pokemon.species_index as usize;
+    let next_species_index: usize = species_index + 1;
     let evolution_line: &[(i32, String); 3] = &subject_pokemon.evolution_line;
-    let can_evolve: bool = (*species_index as usize) + 1 < evolution_line.len();
+    let can_evolve: bool = species_index + 1 < evolution_line.len()
+        && subject_pokemon.level >= evolution_line[next_species_index].0;
 }
 
+fn evolution (subject_pokemon: &mut Pokemon){
+    let oldSpecies: &(i32, String) = subect_pokemon.evolution_line[sub] 
+}
