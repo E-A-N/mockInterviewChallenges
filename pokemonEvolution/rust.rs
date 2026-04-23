6f4d2b24a3a9c8c02501dcf55585bcebae12b6d4
Cyborg_Ean <ean.director@gmail.com>
2026-04-23 13:14:13 -0700

    Update rust.rs

--- pokemonEvolution/rust.rs
+++ pokemonEvolution/rust.rs
@@ -6,13 +6,13 @@ enum ElementTypes {
 }
 
 struct Stats {
-    attack: i32,
-    defense: i32,
-    speed: i32
+    attack: f32,
+    defense: f32,
+    speed: f32,
 }
 
 struct Pokemon {
-    name: String,
+    name: &'static str,
     species_index: usize,
     element_type: ElementTypes,
     level: i32,
@@ -23,14 +23,14 @@ struct Pokemon {
 fn main() {
     println!("Hello, world!");
     let mut poke1 = Pokemon {
-        name: String::from("Pichu"),
+        name: "Pichu",
         species_index: 0,
         element_type: ElementTypes::Lightning,
         level: 0,
         stats: Stats {
-            attack: 1,
-            defense: 1,
-            speed: 3,
+            attack: 1.0,
+            defense: 1.0,
+            speed: 3.0,
         },
         evolution_line: [
             (0, "Pichu"),
@@ -41,14 +41,14 @@ fn main() {
     poke1.level = 1;
     
     let mut poke2 = Pokemon {
-        name: String::from("Bulbasaur"),
+        name: "Bulbasaur",
         species_index: 0,
         element_type: ElementTypes::Lightning,
         level: 0,
         stats: Stats {
-            attack: 1,
-            defense: 1,
-            speed: 3,
+            attack: 1.0,
+            defense: 1.0,
+            speed: 3.0,
         },
         evolution_line: [
             (0, "Bulbasaur"),
@@ -56,9 +56,10 @@ fn main() {
             (32, "Venusaur"),
         ]
     };
-    poke2.level = 2;
+    poke2.level = 15;
     
     level_up(&mut poke1);
+    level_up(&mut poke2);
 }
 
 fn level_up (subject_pokemon: &mut Pokemon) {
@@ -67,15 +68,18 @@ fn level_up (subject_pokemon: &mut Pokemon) {
     }
     
     subject_pokemon.level += 1;
-    subject_pokemon.stats.attack += 1;
-    subject_pokemon.stats.defense += 1;
-    subject_pokemon.stats.speed += 1;
+    subject_pokemon.stats.attack += 1.0;
+    subject_pokemon.stats.defense += 1.0;
+    subject_pokemon.stats.speed += 1.0;
     
     let species_index: usize = subject_pokemon.species_index;
     let next_species_index: usize = species_index + 1;
     let evolution_line: &[(i32, &'static str); 3] = &subject_pokemon.evolution_line;
     let can_evolve: bool = species_index + 1 < evolution_line.len()
         && subject_pokemon.level >= evolution_line[next_species_index].0;
+    if can_evolve {
+        evolution(subject_pokemon);
+    }
 }
 
 fn evolution (subject_pokemon: &mut Pokemon){
@@ -84,6 +88,12 @@ fn evolution (subject_pokemon: &mut Pokemon){
 
     let old_species: &'static str = subject_pokemon.evolution_line[species_index].1;
     let next_species: &'static str = subject_pokemon.evolution_line[next_species_index].1;
+    if subject_pokemon.name == old_species {
+        subject_pokemon.name = next_species;
+    }
 
-    
+    subject_pokemon.species_index += 1;
+    subject_pokemon.stats.attack *= 0.5;
+    subject_pokemon.stats.defense *= 0.5;
+    subject_pokemon.stats.speed *= 0.5;
 }
